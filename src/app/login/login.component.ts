3ba9ed6a28ea3d5c2f4879587b5039b8768223cc
Marvin Senkiv <127746129+marvinsenkiv@users.noreply.github.com>
2023-08-10 20:24:04 -0300

    importacao

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,63 +4,67 @@ import { Router } from '@angular/router';//import do roteamento
 import { DataSharingService } from '../data-sharing.service';
 
 import { ClientDTO } from '../dtos/Client';
+import * as moment from 'moment';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {  
+export class LoginComponent {
 
-  
-  endpointFeed: string= ''; //variavel que irá receber o valor do input
+
+  endpointFeed: string = ''; //variavel que irá receber o valor do input
   errorMessage: string | null = null; //variavel recebe o texto de erro
+  loading: boolean = false;
+  syncMessage = ''
 
   constructor(
-    private service: TecApiService, 
+    private service: TecApiService,
     private router: Router,
     private dataSharingService: DataSharingService
-  ) {}
+  ) { }
 
-  
+
   onLoginButtonClick() {
     this.fetchDataFromApi();
   }
 
   fetchDataFromApi() {
+
     const isCodeClient = /^\d{1,6}$/.test(this.endpointFeed);
-    if(isCodeClient){
-      this.service.getUserByCode(this.endpointFeed).subscribe((data:any)=>{
-        if(data.length){
+    if (isCodeClient) {
+      this.service.getUserByCode(this.endpointFeed).subscribe((data: any) => {
+        if (data.length) {
           const filter = data
           this.dataSharingService.setSharedMultiClient(filter)
           localStorage.setItem('user', JSON.stringify(filter));
           this.router.navigate(['/inicio']);
         }
       },
-      error => {
-        console.error('error', error);
-        this.errorMessage = 'Ocorreu um erro no sistema';//armazena a mensagem de erro na variavel        
-      }
+        error => {
+          console.error('error', error);
+          this.errorMessage = 'Ocorreu um erro no sistema';//armazena a mensagem de erro na variavel        
+        }
       )
     }
     else {
       this.service.getUserData(this.endpointFeed).subscribe(
-        (data: any[]) => {        
-          console.log('Resultado API',data);
+        (data: any[]) => {
+          console.log('Resultado API', data);
           // @ts-ignore
           if (data.contato) {
             // @ts-ignore
             this.dataSharingService.setSharedMultiClient([data.contato])
             // @ts-ignore
             localStorage.setItem('user', JSON.stringify([data.contato]));
-  
+
             this.router.navigate(['/inicio']);
           } else if (data.length) {
             const filter = data.map(item => item.contato);
             this.dataSharingService.setSharedMultiClient(filter)
             localStorage.setItem('user', JSON.stringify(filter));
             this.router.navigate(['/inicio']);
-  
+
           } else {
             this.errorMessage = '*Email ou documento não cadastrado'
           }
@@ -71,7 +75,26 @@ export class LoginComponent {
         }
       );
     }
-   
+
+  }
+
+  sync() {
+    const today = moment();
+
+    // Criando a startDate como hoje - 1 dia
+    const startDate = moment(today).subtract(1, 'days');
+
+    // Formatando as datas no formato "DD/MM/YYYY"
+    const startDateString = startDate.format('DD/MM/YYYY');
+    const finalDateString = today.format('DD/MM/YYYY');
+    this.loading = true;
+    this.service.importacaoJob(startDateString, finalDateString).subscribe((data) => {
+      this.syncMessage = data.response;
+      this.loading = false
+    }, (error) => {
+      console.log('e', error);
+      this.loading = false
+    })
   }
 
 }
--- src/app/tec-api.service.ts
+++ src/app/tec-api.service.ts
@@ -45,9 +45,18 @@ export class TecApiService {
   schedule(request:any): Observable<any> {
     //const endpointUrl = `${this.baseUrl}/api/blingUserData/marvinsenkiv@gmail.com`; // Substitua 'seu-endpoint' pelo caminho correto
     const endpointUrl = `${this.baseUrl}/symplebook/create/schedule?date=${request.date}&service_id=${request.serviceId}&period=${request.period}&document=${request.doc}&end=${request.selectedEnd} `;
-    console.log('??', endpointUrl)
     return this.http.get<any>(endpointUrl);
   }
+  
+  importacaoJob(startDate: string, finalDate: string): Observable<any> {
+    const endpointUrl = `${this.baseUrl}/job/importacao`;
+    const requestData = {
+      date: startDate,
+      finalDate: finalDate
+    };
 
+    // Fazendo uma solicitação POST para a rota /job/importacao com o payload especificado
+    return this.http.post<any>(endpointUrl, requestData);
+  }
 
 }
